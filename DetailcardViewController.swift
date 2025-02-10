664b1e53cfe7ff6490f7a8bbc36af2b2cad4f3ff
a23i127 <a23i127ne@ous.jp>
2025-02-10 18:15:13 +0900

    自動関連探索の途中

--- DetailcardViewController.swift
+++ DetailcardViewController.swift
@@ -7,25 +7,91 @@
 
 import Foundation
 import UIKit
-class DetailCard: UIViewController {
+class DetailCard: UIViewController,UICollectionViewDelegateFlowLayout,UICollectionViewDataSource,UICollectionViewDelegate {
     var cardData: cards?
+    var anyCards: [cards]?
     @IBOutlet weak var imageView: UIImageView!
-    
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var collectionView2: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = cardData?.trueName
         ImageUrl()
     }
     override func viewDidAppear(_ animated: Bool) {
-        
+        let fetchCardInstance = fetchCardData()
+        fetchCardInstance.fecthCard() { [weak self] cards,result in
+            guard let self = self else { return }
+            switch result {
+            case true:
+                self.anyCards = cards
+                self.collectionView2.reloadData()
+                //更新
+            case false: self.showAlert()
+            }
+        }
+    }
+    func conditionalSearch() {
+        let fetchCardInstance = fetchCardData()
+        fetchCardInstance.conditionalSearch(card: cardData) { [weak self] cards,result in
+                guard let self = self else { return }
+                switch result {
+                case true:
+                    self.anyCards = cards
+                    self.collectionView2.reloadData()
+                    //更新
+                case false: self.showAlert()
+                }
+        }
     }
     func ImageUrl() {
         //ここで、urlを画像に表示させる
         do{
-            imageView.sd_setImage(with:UserInfo.shared.imageUrls![UserInfo.shared.indexPath])
+            let url = URL(string: cardData!.imageUrl)
+            imageView.sd_setImage(with: url)
         } catch {
             print("Error : Cat't get image")
         }
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        //今回はとりあえず12とする。（配列に表示させたいデータを入れている場合は配列のデータ数を返せば良い。）
+        return anyCards?.count ?? 0
+    }
+    //セルに表示する内容を記載する
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //storyboard上のセルを生成　storyboardのIdentifierで付けたものをここで設定する
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "anyCards", for:  indexPath)
+        //セル上のTag(1)とつけたUILabelを生成
+        let cardObj = anyCards?[indexPath.row]
+        let castingCell = cell as? CardCell
+        guard let castingCell else {
+            self.showAlert()
+            return  cell }
+        UserInfo.shared.indexPath = indexPath.row
+        castingCell.configure2(card:cardObj)
+        //今回は簡易的にセルの番号をラベルのテキストに反映させる
+        return cell
+    }
+    //セルのサイズを指定する処理
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let padding: CGFloat = 10 // セル間の間隔
+        let numberOfItemsPerRow: CGFloat = 4 // 1行に表示するセルの数
+        let totalPadding = padding * (numberOfItemsPerRow + 1)//全体の余白を計算
+        //Viewの中に表示させるcellの大きさを指定
+        let individualWidth = (collectionView.frame.width - totalPadding) / numberOfItemsPerRow
+        return CGSize(width: individualWidth, height: individualWidth+20) // 長方形のセル
+    }
+    //セル選択時の処理
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        UserInfo.shared.indexPath = indexPath.row
+        self.cardData = self.anyCards![indexPath.row]
+        self.ImageUrl()
+        self.conditionalSearch()
+        self.collectionView2.reloadData()
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "お知らせです", message: "エラーです", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
--- fetchCardApi.swift
+++ fetchCardApi.swift
@@ -23,4 +23,19 @@ class fetchCardData {
             }
         }
     }
+    func conditionalSearch(card:cards?,completion: @escaping ([cards]?, Bool) -> Void) {
+        AF.request("http://127.0.0.1:8080/api/card",method: .put,parameters: ["searchTag":card?.trueName],encoding: JSONEncoding.default).response { response in
+            guard let data = response.data else { return }
+           
+            let decoder = JSONDecoder()
+            do {
+               let decodedObject = try decoder.decode([cards].self, from: data)
+                completion(decodedObject, true)
+            }
+            catch {
+                print("読み込み失敗")
+                completion(nil,false)
+            }
+        }
+    }
 }
