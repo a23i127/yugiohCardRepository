268791dd07c812f5c4074cad93d2f99a18dcdb51
a23i127 <a23i127ne@ous.jp>
2025-04-28 23:37:04 +0900

    通信未完成

--- CinkuroJokyuVIewController.swift
+++ CinkuroJokyuVIewController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import Alamofire
 class CinkuroJokyuVIewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var imageView2: UIImageView!
@@ -19,18 +20,33 @@ class CinkuroJokyuVIewController: UIViewController {
     @IBOutlet weak var textView5: UITextView!
     @IBOutlet weak var textView6: UITextView!
     @IBOutlet weak var textView7: UITextView!
+    @IBOutlet weak var textVIew8: UITextView!
     @IBOutlet weak var textViewHeihtConst: NSLayoutConstraint!
     @IBOutlet weak var textViewHeihtConst2: NSLayoutConstraint!
     @IBOutlet weak var textViewHeihtConst3: NSLayoutConstraint!
+    @IBOutlet weak var textView4HeightConst4: NSLayoutConstraint!
+    @IBOutlet weak var textView4HeightConst5: NSLayoutConstraint!
+    @IBOutlet weak var textView4HeightConst6: NSLayoutConstraint!
+    @IBOutlet weak var textView4HeightConst7: NSLayoutConstraint!
+    @IBOutlet weak var textView4HeightConst8: NSLayoutConstraint!
+    var kategori: String?
+    var databox: [introduce]?
+    var textViews: [UITextView] = []
+    var imageViews: [UIImageView] = []
+    var textViewConst: [NSLayoutConstraint] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     override func viewDidAppear(_ animated: Bool) {
+        print(kategori)
+        fetchData()
         super.viewDidAppear(animated)
-        let textViews = [textView1,textView2,textView3,textView4,textView5,textView6,textView7]
+        textViews = [textView1,textView2,textView3,textView4,textView5,textView6,textView7,textVIew8]
+        imageViews = [imageView,imageView2,imageView3,imageView4]
+        textViewConst = [textViewHeihtConst,textViewHeihtConst,textViewHeihtConst2,textView4HeightConst4,textView4HeightConst5,textView4HeightConst6,textView4HeightConst7,textView4HeightConst8]
         for tv in textViews {
-            textViewsCustom(textView: tv!)
+            textViewsCustom(textView: tv)
         }
         zoomToRedLineAreaSmooth(imageView: imageView)
     }
@@ -58,15 +74,56 @@ class CinkuroJokyuVIewController: UIViewController {
         textView.textContainerInset = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 16) // 内側の余白
         textView.isEditable = false
     }
+    func fetchData() {
+        let fetchDataObj = fetchTextData()
+        fetchDataObj.fecthIntroduceData(kategori: kategori!) { [weak self] decodedObj,bool in
+            guard let self = self else { return }
+            switch bool {
+            case true:
+                self.databox = decodedObj
+                self.Dataset(texts: self.databox!)
+            case false:
+                self.showAlert()
+            }
+        }
+    }
+    func Dataset(texts: [introduce]) {
+        for textbox in texts {
+            let position = textbox.position
+            if let textData = textbox.text {
+                switch position {
+                case 0..<textViews.count:
+                    textViews[position].text = textData
+                default:
+                    break
+                }
+            }
+            
+            if let photoString = textbox.image {
+                switch position {
+                case 0..<imageViews.count:
+                    urlImageSet(imageView: imageViews[position], photoData: photoString)
+                default:
+                    break
+                }
+            }
+        }
+    }
+    func urlImageSet(imageView:UIImageView,photoData: String) {
+        print(photoData)
+        let url = URL(string: photoData)
+        do {
+            imageView.sd_setImage(with:url)
+        } catch {
+            print("Error : Cat't get image")
+        }
+    }
     func zoomToRedLineAreaSmooth(imageView: UIImageView) {
-        // 表示したい赤線部分の範囲（割合で指定）
-        // 例として：中央下部の横長部分（画像サイズに応じて調整必要）
         let targetRect = CGRect(x: 0.05, y: 0.9, width: 0.6, height: 0.1)
-        // アニメーション付きで contentsRect を変更
         let animation = CABasicAnimation(keyPath: "contentsRect")
         animation.fromValue = imageView.layer.contentsRect
         animation.toValue = targetRect
-        animation.duration = 5.0
+        animation.duration = 8.0
         animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
         animation.autoreverses = true             // 元に戻す
         animation.repeatCount = .infinity         // 無限ループ！
@@ -79,7 +136,9 @@ class CinkuroJokyuVIewController: UIViewController {
             imageView.transform = .identity
         }
     }
-    @IBAction func backAction(_ sender: Any) {
-        self.dismiss(animated: true)
+    func showAlert() {
+        let alert = UIAlertController(title: "お知らせです", message: "エラーです", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
