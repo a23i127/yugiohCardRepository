55b79a0ab441ccbaa95286df3e896f017e85cc44
a23i127 <a23i127ne@ous.jp>
2025-05-01 16:46:18 +0900

    通信完成

--- IntroducrVIewController.swift
+++ IntroducrVIewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 import Alamofire
-class CinkuroJokyuVIewController: UIViewController {
+class introduceViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var imageView2: UIImageView!
     @IBOutlet weak var imageView3: UIImageView!
@@ -20,7 +20,7 @@ class CinkuroJokyuVIewController: UIViewController {
     @IBOutlet weak var textView5: UITextView!
     @IBOutlet weak var textView6: UITextView!
     @IBOutlet weak var textView7: UITextView!
-    @IBOutlet weak var textVIew8: UITextView!
+    @IBOutlet weak var textView8: UITextView!
     @IBOutlet weak var textViewHeihtConst: NSLayoutConstraint!
     @IBOutlet weak var textViewHeihtConst2: NSLayoutConstraint!
     @IBOutlet weak var textViewHeihtConst3: NSLayoutConstraint!
@@ -29,11 +29,20 @@ class CinkuroJokyuVIewController: UIViewController {
     @IBOutlet weak var textView4HeightConst6: NSLayoutConstraint!
     @IBOutlet weak var textView4HeightConst7: NSLayoutConstraint!
     @IBOutlet weak var textView4HeightConst8: NSLayoutConstraint!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var parentView1: UIView!
+    @IBOutlet weak var parentView2: UIView!
+    @IBOutlet weak var parentView3: UIView!
+    @IBOutlet weak var zoomButton1: UIButton!
+    @IBOutlet weak var zoomButton2: UIButton!
+    @IBOutlet weak var zoomButton3: UIButton!
+    
     var kategori: String?
-    var databox: [introduce]?
+    var databox: [texts]?
     var textViews: [UITextView] = []
     var imageViews: [UIImageView] = []
-    var textViewConst: [NSLayoutConstraint] = []
+    var isZoomedIn = false
+    var constraintMap:[UITextView:NSLayoutConstraint] = [:]
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -42,28 +51,33 @@ class CinkuroJokyuVIewController: UIViewController {
         print(kategori)
         fetchData()
         super.viewDidAppear(animated)
-        textViews = [textView1,textView2,textView3,textView4,textView5,textView6,textView7,textVIew8]
+        textViews = [textView1,textView2,textView3,textView4,textView5,textView6,textView7,textView8]
         imageViews = [imageView,imageView2,imageView3,imageView4]
-        textViewConst = [textViewHeihtConst,textViewHeihtConst,textViewHeihtConst2,textView4HeightConst4,textView4HeightConst5,textView4HeightConst6,textView4HeightConst7,textView4HeightConst8]
-        for tv in textViews {
-            textViewsCustom(textView: tv)
+        constraintMap = [textView1: textViewHeihtConst,
+                         textView2: textViewHeihtConst2,
+                         textView3: textViewHeihtConst3,
+                         textView4: textView4HeightConst4,
+                         textView5: textView4HeightConst5,
+                         textView6: textView4HeightConst6,
+                         textView7: textView4HeightConst7,
+                         textView8: textView4HeightConst8]
+        parentView1.bringSubviewToFront(zoomButton1)
+        parentView2.bringSubviewToFront(zoomButton2)
+        parentView3.bringSubviewToFront(zoomButton3)
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+            for tv in self.textViews {
+                textViewsCustom(textView: tv)
+            
         }
-        zoomToRedLineAreaSmooth(imageView: imageView)
     }
     func textViewsCustom(textView: UITextView) {
-        let fittingSize = CGSize(width: textView.frame.width, height: .greatestFiniteMagnitude)
-        let size = textView.sizeThatFits(fittingSize)
-        switch textView {
-        case textView1:
-            textViewHeihtConst.constant = size.height
-        case textView3:
-            textViewHeihtConst2.constant = size.height
-        case textView6:
-            textViewHeihtConst3.constant = size.height
-        default:
-            break
-        }
-        textView.isScrollEnabled = false
+        
+        let height = textView.sizeThatFits(CGSize(width: textView.frame.size.width, height: CGFloat.greatestFiniteMagnitude)).height
+        if let constraint = constraintMap[textView] {
+                constraint.constant = height
+            }
         textView.layer.cornerRadius = 12
         textView.backgroundColor = UIColor(white: 1.0, alpha: 0.8) // 半透明でクール
         textView.layer.cornerRadius = 12
@@ -73,10 +87,11 @@ class CinkuroJokyuVIewController: UIViewController {
         
         textView.textContainerInset = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 16) // 内側の余白
         textView.isEditable = false
+        textView.isScrollEnabled = false
     }
     func fetchData() {
         let fetchDataObj = fetchTextData()
-        fetchDataObj.fecthIntroduceData(kategori: kategori!) { [weak self] decodedObj,bool in
+        fetchDataObj.fecthViewData(kategori: kategori!, dataContent: "サブデータ") { [weak self] decodedObj,bool in
             guard let self = self else { return }
             switch bool {
             case true:
@@ -87,7 +102,7 @@ class CinkuroJokyuVIewController: UIViewController {
             }
         }
     }
-    func Dataset(texts: [introduce]) {
+    func Dataset(texts: [texts]) {
         for textbox in texts {
             let position = textbox.position
             if let textData = textbox.text {
@@ -99,7 +114,7 @@ class CinkuroJokyuVIewController: UIViewController {
                 }
             }
             
-            if let photoString = textbox.image {
+            if let photoString = textbox.photo {
                 switch position {
                 case 0..<imageViews.count:
                     urlImageSet(imageView: imageViews[position], photoData: photoString)
@@ -108,9 +123,9 @@ class CinkuroJokyuVIewController: UIViewController {
                 }
             }
         }
+        
     }
     func urlImageSet(imageView:UIImageView,photoData: String) {
-        print(photoData)
         let url = URL(string: photoData)
         do {
             imageView.sd_setImage(with:url)
@@ -118,18 +133,34 @@ class CinkuroJokyuVIewController: UIViewController {
             print("Error : Cat't get image")
         }
     }
-    func zoomToRedLineAreaSmooth(imageView: UIImageView) {
-        let targetRect = CGRect(x: 0.05, y: 0.9, width: 0.6, height: 0.1)
-        let animation = CABasicAnimation(keyPath: "contentsRect")
-        animation.fromValue = imageView.layer.contentsRect
-        animation.toValue = targetRect
-        animation.duration = 8.0
-        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
-        animation.autoreverses = true             // 元に戻す
-        animation.repeatCount = .infinity         // 無限ループ！
-        imageView.layer.add(animation, forKey: "zoomContentsRect")
-        // 実際の表示も変える
-        imageView.layer.contentsRect = targetRect
+    func zoomToRedLineAreaSmooth(imageView: UIImageView,toggle: Bool) {
+            let targetRect = CGRect(x: 0.01, y: 0.9, width: 0.8, height: 0.1)
+            let zoomOutRect = CGRect(x: 0, y: 0, width: 1, height: 1)
+            let animation = CABasicAnimation(keyPath: "contentsRect")
+            animation.fromValue = imageView.layer.contentsRect //可変の値
+            animation.toValue = toggle ? targetRect : zoomOutRect
+            animation.duration = 2.0
+            animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+            imageView.layer.add(animation, forKey: "zoomContentsRect")
+            // 実際の表示も変える
+        imageView.layer.contentsRect = toggle ? targetRect : zoomOutRect
+        
+    }
+    @IBAction func imageDidTatch(_ sender: UIButton) {
+        // 今の画像名に応じて切り替え
+        isZoomedIn.toggle()
+        let imageName = isZoomedIn ? "minus.magnifyingglass" : "plus.magnifyingglass"
+        sender.setImage(UIImage(systemName: imageName), for: .normal)
+        if let parentView = (sender as AnyObject).superview {
+            for subview in parentView!.subviews {
+                    if let imageView = subview as? UIImageView {
+                        // ここでimageViewを取り出せた！
+                        zoomToRedLineAreaSmooth(imageView: imageView,toggle: isZoomedIn)
+                        // たとえばここで何かする
+                       
+                    }
+                }
+            }
     }
     func resetZoom(imageView: UIImageView) {
         UIView.animate(withDuration: 0.8) {
--- TestViewController.swift
+++ TestViewController.swift
@@ -9,19 +9,15 @@ import Foundation
 import UIKit
 import PKHUD
 import SDWebImage
-class TestViewController: UIViewController {
+class TestViewController: UIViewController,UIScrollViewDelegate {
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentView: UIView!
-    @IBOutlet weak var exsMonster: UIImageView!
-    @IBOutlet weak var exsMonsterArm: UIImageView!
     @IBOutlet weak var textView1: UITextView!
     @IBOutlet weak var textView2: UITextView!
     @IBOutlet weak var textView3: UITextView!
     @IBOutlet weak var textView4: UITextView!
     @IBOutlet weak var textView5: UITextView!
-    @IBOutlet weak var monsterStackView: UIStackView!
-    @IBOutlet weak var monsterStackView2: UIStackView!
     @IBOutlet weak var imageView1: UIImageView!
     @IBOutlet weak var imageView2: UIImageView!
     @IBOutlet weak var imageView3: UIImageView!
@@ -33,7 +29,21 @@ class TestViewController: UIViewController {
     @IBOutlet weak var imageView9: UIImageView!
     @IBOutlet weak var buttun1: UIButton!
     @IBOutlet weak var buttun2: UIButton!
+    @IBOutlet weak var stackView1: UIStackView!
+    @IBOutlet weak var stackView2: UIStackView!
+    @IBOutlet weak var stackVIew3: UIStackView!
+    var animationFlags: [Bool] = Array(repeating: false, count: 8)
     var textData:[texts]?
+    var kategori: String?
+    var textViews: [UITextView] = []
+    var buttons: [UIButton] = []
+    var imageViews: [UIImageView] = []
+    struct AnimationAction {
+        let threshold: CGFloat
+        var flag: Bool
+        let action: () -> Void
+    }
+    var animationAction: [AnimationAction] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundImage.contentMode = .scaleAspectFill
@@ -42,101 +52,74 @@ class TestViewController: UIViewController {
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        HUD.show(.progress, onView: self.view)
-        let textViews: [UITextView] = [textView1,textView2,textView3]
-        fetchTextActon()
-        backgroundImage.frame = CGRect(origin: .zero, size: scrollView.contentSize)
-        
+        textViews = [textView1,textView2,textView3,textView4,textView5]
+        buttons = [buttun1, buttun2]
+        imageViews = [imageView1, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7, imageView8, imageView9]
+        if textData == nil {
+            HUD.show(.progress, onView: self.view)
+            fetchTextActon()
+        }
         for tv in textViews {
             textViewsCustom(textView: tv)
         }
-        //animateCharacter(monster: exsMonster, color: .yellow)
-        //animateHand()
+        
+        animationAction = [
+            AnimationAction(threshold: 0.02, flag: false, action: { self.appearView(on: self.textView1) }),
+            AnimationAction(threshold: 0.02, flag: false, action: {
+                self.zoomToRedLineAreaSmooth(imageView: self.imageView1)
+                self.appearView(on: self.stackView1)
+            }),
+            AnimationAction(threshold: 0.1, flag: false, action: { self.appearView(on: self.stackView2) }),
+            AnimationAction(threshold: 0.35, flag: false, action: { self.animateCharacter(monster: self.imageView4, color: .yellow) }),
+            AnimationAction(threshold: 0.45, flag: false, action: { self.animateCharacter(monster: self.imageView5, color: .yellow) }),
+            AnimationAction(threshold: 0.65, flag: false, action: { self.kategoriAction()}),
+            AnimationAction(threshold: 0.9, flag: false, action: { self.appearView(on: self.stackVIew3) })
+        ]
     }
     func fetchTextActon() {
-        if textData == nil {
-            let fetchTextInstance = fetchTextData()
-            fetchTextInstance.fecthText() { [weak self] texts,result in
-                guard let self = self else { return }
-                switch result {
-                case true:
-                    for item in texts! {
-                        UserInfo.shared.photoUrls?.append(URL(string:item.photo!)!)
-                    }
-                    SDWebImagePrefetcher.shared.prefetchURLs(UserInfo.shared.imageUrls!)//プリロード
-                    self.textData = texts!
-                    DispatchQueue.main.async{
-                        self.textDataset(texts: self.textData!)
-                    }
-                case false: self.showAlert()
-                }
+        guard let kategori = kategori else { return showAlert() }
+        let fetchTextInstance = fetchTextData()
+        fetchTextInstance.fecthViewData(kategori: kategori,dataContent: "メインデータ") { [weak self] texts,result in
+            guard let self = self else { return }
+            switch result {
+            case true:
+                self.textData = texts!
+                self.Dataset(texts: self.textData!)
+            case false:
+                self.showAlert()
             }
         }
     }
-    func textDataset(texts: [texts]) {
+    func Dataset(texts: [texts]) {
         for textbox in texts {
-            switch textbox.position {
-            case 0:
-                if let textData = textbox.text  {
-                    textView1.text = textData
+            let position = textbox.position
+            if let textData = textbox.text {
+                switch position {
+                case 0..<textViews.count:
+                    textViews[position].text = textData
+                case 5:
+                    buttons[0].setTitle(textData, for: .normal)
+                case 6:
+                    buttons[1].setTitle(textData, for: .normal)
+                default:
+                    break
                 }
-                if let photoString = textbox.photo  {
-                    urlImageSet(imageView: imageView1, photoData: photoString)
-                }
-            case 1:
-                if let textData = textbox.text  {
-                    textView2.text = textData
-                }
-                if let photoString = textbox.photo  {
-                    urlImageSet(imageView: imageView2, photoData: photoString)
-                }
-            case 2:
-                if let textData = textbox.text  {
-                    textView3.text = textData
-                }
-                if let photoString = textbox.photo  {
-                    urlImageSet(imageView: imageView3, photoData: photoString)
-                }
-            case 3:
-                if let textData = textbox.text  {
-                    textView4.text = textData
-                }
-                if let photoString = textbox.photo  {
-                    urlImageSet(imageView: imageView3, photoData: photoString)
-                }
-            case 4:
-                if let textData = textbox.text  {
-                    textView5.text = textData
-                }
-                if let photoString = textbox.photo  {
-                    urlImageSet(imageView: imageView3, photoData: photoString)
-                }
-            case 5:
-                if let textData = textbox.text  {
-                    buttun1.setTitle(textData, for: .normal)
-                }
-                if let photoString = textbox.photo  {
-                    urlImageSet(imageView: imageView3, photoData: photoString)
-                }
-            case 6:
-                if let textData = textbox.text  {
-                    buttun2.setTitle(textData, for: .normal)
-                }
-                if let photoString = textbox.photo  {
-                    urlImageSet(imageView: imageView3, photoData: photoString)
-                }
-            case 7: if let photoString = textbox.photo  {
-                urlImageSet(imageView: imageView3, photoData: photoString)
             }
-            case 8: if let photoString = textbox.photo  {
-                urlImageSet(imageView: imageView3, photoData: photoString)
-            }
-            default:
-                break
+            
+            if let photoString = textbox.photo {
+                switch position {
+                case 0..<imageViews.count:
+                    urlImageSet(imageView: imageViews[position], photoData: photoString)
+                case 9:
+                    urlImageSet(imageView: backgroundImage, photoData: photoString)
+                default:
+                    break
+                }
             }
         }
     }
     func urlImageSet(imageView:UIImageView,photoData: String) {
+        print(photoData)
         let url = URL(string: photoData)
         do {
             imageView.sd_setImage(with:url)
@@ -156,13 +139,108 @@ class TestViewController: UIViewController {
         textView.textContainerInset = UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 16) // 内側の余白
         textView.isEditable = false
     }
-    
-    func animateCharacter(monster: UIImageView, color: UIColor) {
-        // 斜め右下に向かう位置に制約を変える
-        monster.transform = CGAffineTransform.identity
-        UIView.animate(withDuration: 1.0) {
-            monster.alpha = 1  // 徐々に表示される
+    func zoomToRedLineAreaSmooth(imageView: UIImageView) {
+        var targetRect = CGRect(x: 0.6, y: 0.1, width: 0.3, height: 0.1)
+        if imageView == imageView3 {
+            targetRect = CGRect(x: 0.05, y: 0.75, width: 0.6, height: 0.1)
         }
+        //Uikitで座標反転する！
+        let animation = CABasicAnimation(keyPath: "contentsRect")
+        animation.fromValue = imageView.layer.contentsRect
+        animation.toValue = targetRect
+        animation.duration = 1.0
+        animation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)// 無限ループ！
+        imageView.layer.add(animation, forKey: "zoomContentsRect")
+        // 実際の表示も変える
+        imageView.layer.contentsRect = targetRect
+    }
+    func kategoriAction(){
+        switch kategori {
+        case "エクシーズ":
+            exsSummon(imageView1: self.imageView4, imageView2: self.imageView5)
+        default:
+            break
+        }
+    }
+    //カテゴリ専用関数
+    func exsSummon(imageView1: UIImageView, imageView2: UIImageView) {
+        let screenWidth = contentView.bounds.width
+        let screenHeight = contentView.bounds.height
+        let targetCenter = CGPoint(x: screenWidth / 2, y: screenHeight * 0.3)
+        UIView.animate(withDuration: 1.0, delay: 0, options: [.curveEaseOut], animations: {
+            imageView1.alpha = 1.0
+        }, completion: { _ in
+            // imageView2を少し遅れてフェードイン＆移動
+            UIView.animate(withDuration: 1.0, delay: 0.2, options: [.curveEaseOut], animations: {
+                imageView2.alpha = 1.0
+            }, completion: {_ in
+                UIView.animate(withDuration: 2, delay: 0, options: [.curveEaseOut,], animations: {
+                    imageView1.center = targetCenter
+                    imageView2.center = targetCenter
+                }, completion: { _ in
+                    let target = CGPoint(x: screenWidth / 2, y: screenHeight * 0.7)
+                    self.firstGlowAnimation(point: target)
+                    self.explosion(at: target, in: self.contentView)
+                })
+            })
+        })
+    }
+    func firstGlowAnimation(point: CGPoint) {
+        let glow = CALayer()
+        glow.backgroundColor = UIColor.black.cgColor
+        glow.frame = CGRect(x: point.x - 40, y: point.y - 40, width: 80, height: 80)
+        glow.cornerRadius = 40
+        glow.opacity = 0
+        view.layer.addSublayer(glow)
+        
+        let glowAnimation = CABasicAnimation(keyPath: "opacity")
+        glowAnimation.fromValue = 0
+        glowAnimation.toValue = 1
+        glowAnimation.duration = 0.2
+        glowAnimation.autoreverses = true
+        glowAnimation.repeatCount = 3
+        glow.add(glowAnimation, forKey: "glowFlash")
+    }
+    func explosion(at point: CGPoint, in view: UIView) {
+        createExplosion(point: point, color: .yellow, delay: 0.6)
+        createExplosion(point: point, color: .black, delay: 0.9,additionalAnimations: {
+            self.imageView4.alpha = 0
+            self.imageView5.alpha = 0
+        },completion: {
+            self.animateCharacter(monster: self.imageView7, color: .yellow)
+            self.animateHand()
+        } )
+    }
+    func createExplosion(point: CGPoint,color: UIColor, delay: TimeInterval, additionalAnimations: (() -> Void)? = nil, completion: (() -> Void)? = nil) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                let explosion = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+                explosion.center = point
+                explosion.layer.cornerRadius = 10
+                explosion.backgroundColor = color
+                self.contentView.addSubview(explosion)
+                UIView.animate(withDuration: 0.5, animations: {
+                    explosion.transform = CGAffineTransform(scaleX: 100, y: 100)
+                    explosion.alpha = 0
+                    additionalAnimations?()
+                }, completion: { _ in
+                    completion?()
+                })
+            }
+        }
+    func animateHand() {
+        self.auraPlas(imageView: imageView8, color: .yellow)
+        appearView(on: imageView8)
+        UIView.animate(withDuration: 2.5,
+                       delay: 0,
+                       options: [.autoreverse, .repeat],
+                       animations: {
+            self.imageView8.transform = CGAffineTransform(translationX: 0, y: -5)
+        }, completion: nil)
+    }
+    //汎用関数
+    func animateCharacter(monster: UIImageView, color: UIColor) {
+        monster.transform = CGAffineTransform.identity
+        appearView(on: monster)
         UIView.animate(
             withDuration: 1.0,
             delay: 0,
@@ -173,17 +251,7 @@ class TestViewController: UIViewController {
                 self.auraPlas(imageView: monster, color: color)
                 self.floatAction(imageView: monster)
             },
-            completion: {_ in
-            }
-        )
-    }
-    func animateHand() {
-        UIView.animate(withDuration: 3.5,
-                       delay: 0,
-                       options: [.autoreverse, .repeat],
-                       animations: {
-            self.exsMonsterArm.transform = CGAffineTransform(translationX: 0, y: -5)
-        }, completion: nil)
+            completion: nil)
     }
     func auraPlas(imageView: UIImageView,color: UIColor) {
         imageView.layer.shadowColor = color.cgColor     // オーラの色
@@ -209,41 +277,17 @@ class TestViewController: UIViewController {
         float.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
         imageView.layer.add(float, forKey: "float")
     }
-    /* func scrollViewDidScroll(_ scrollView: UIScrollView) {
-     let offsetY = scrollView.contentOffset.y
-     let viewHeight = contentView.frame.height
-     let scrollRatio = offsetY / viewHeight  // ← これがポイント！
-     switch true {
-     case scrollRatio > 0.1 && !animationFlag:
-     self.appearView( on: self.apealView)
-     animationFlag = true
-     case scrollRatio > 0.2 && !animationFlag2:
-     self.appearView( on: self.textView8)
-     animationFlag2 = true
-     case scrollRatio > 0.3 && !animationFlag3:
-     self.appearView( on: self.textView3)
-     animationFlag3 = true
-     case scrollRatio > 0.4 && !animationFlag4:
-     jankuSinkuron.alpha = 1
-     jankuSpeader.alpha = 1
-     changeClearColor(monsterView: jankuSinkuron) {
-     self.lebel3Image.alpha = 0
-     self.tyunarLabel.alpha = 0
-     }
-     changeClearColor(monsterView: jankuSpeader) {
-     self.lebel5Image.alpha = 0
-     self.monstarLabel.alpha = 0
-     }
-     DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-     self.tyunar1.alpha = 0
-     self.tyunar2.alpha = 0
-     self.animateSynchroLineUp()
-     }
-     animationFlag4 = true
-     default:
-     break
-     }
-     }*/
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let scrollableHeight = scrollView.contentSize.height - scrollView.frame.height
+        let scrollRatio = offsetY / scrollableHeight
+        for i in 0..<animationAction.count {
+            if scrollRatio > animationAction[i].threshold && !animationAction[i].flag {
+                animationAction[i].action()
+                animationAction[i].flag = true
+            }
+        }
+    }
     func appearView(on textView: UIView) {
         UIView.animate(withDuration: 0.3,animations: {
             textView.alpha = 1  // 徐々に表示される
@@ -253,5 +297,9 @@ class TestViewController: UIViewController {
         let alert = UIAlertController(title: "お知らせです", message: "エラーです", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextViewData = segue.destination as! introduceViewController
+        nextViewData.kategori = kategori!
     }
 }
